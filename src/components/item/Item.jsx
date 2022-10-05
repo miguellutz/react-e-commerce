a3473a1b5dc2de74d7fc4584aacf4474b729f4f4
Miguel Lutz <miguellutz31@gmail.com>
2022-10-05 10:39:01 +0200

    Rating component for items

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './item.css'
 
+import { BsCart } from 'react-icons/bs'
+
+import Rating from '../rating/Rating'
+
 function Item() {
   return (
     <div className="item__container">
@@ -8,7 +12,14 @@ function Item() {
         <img src={ require('../../assets/iphone.jpeg')} alt="iphone" />
       </div>
       <div className="item__content">
-        <h2>iPhone 13</h2>
+        <div className="item__info">
+          <h2>iPhone 13</h2>
+          <Rating />
+          <p>$899</p>
+        </div>
+        <div className="cart__container">
+          <BsCart />
+        </div>
       </div>
     </div>
   )
